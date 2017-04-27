9968b63a20e95af7c39d8eb147c96b34e1d525d4
Kevin <kevinvdbosch_1@hotmail.com>
2017-04-27 12:52:45 +0200

    Added background

--- src/menu/Fallout4MenuApp.java
+++ src/menu/Fallout4MenuApp.java
@@ -3,6 +3,9 @@ package menu;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -11,6 +14,16 @@ public class Fallout4MenuApp extends Application {
 	private Parent createContent() {
 		Pane root = new Pane();
 		
+		ImageView imageView = new ImageView(new Image(getClass()
+				.getResource("res/Fallout4_bg.jpg").toExternalForm()));
+		
+		imageView.setFitWidth(1280);
+		imageView.setFitHeight(720);
+		
+		SepiaTone tone = new SepiaTone(0.85);
+		imageView.setEffect(tone);
+		
+		root.getChildren().addAll(imageView);
 		return root;
 	}
 	
